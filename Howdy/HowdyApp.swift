ae53fb1907dcc20cf9368e719c78b17fbe02657d
Rashon <rashyslop@outlook.com>
2023-10-18 19:02:16 -0600

    Initial Commit

---
+++ Howdy/HowdyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowdyApp.swift
+//  Howdy
+//
+//  Created by Rashon Hyslop on 10/18/23.
+//
+
+import SwiftUI
+
+@main
+struct HowdyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
