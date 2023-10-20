9c230660043d1a67a1b0ee22babc6fe98a520b88
Rashon <rashyslop@outlook.com>
2023-10-20 15:21:41 -0600

    saving from 10/20

---
+++ Howdy/Views/Custom Tab bar/CustomTabBar.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTabBar.swift
+//  Howdy
+//
+//  Created by Rashon Hyslop on 10/19/23.
+//
+
+import SwiftUI
+
+struct CustomTabBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomTabBar()
+}
