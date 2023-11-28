8bf758b9f7a0d797739f206739cc04657f5eebfa
Rashon <rashyslop@outlook.com>
2023-11-27 20:27:46 -0700

    Monday update

---
+++ Howdy/Views/Main/CommentView.swift
@@ -0,0 +1,18 @@
+//
+//  CommentView.swift
+//  Howdy
+//
+//  Created by Rashon Hyslop on 11/27/23.
+//
+
+import SwiftUI
+
+struct CommentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CommentView()
+}
