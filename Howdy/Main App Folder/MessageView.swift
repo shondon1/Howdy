01c139228a5285e93ed9de7b7bc8747b5defbb0d
Rashon <rashyslop@outlook.com>
2024-01-23 21:55:09 -0700

    1/23

---
+++ Howdy/Main App Folder/MessageView.swift
@@ -0,0 +1,18 @@
+//
+//  MessageView.swift
+//  Howdy
+//
+//  Created by Rashon Hyslop on 1/22/24.
+//
+
+import SwiftUI
+
+struct MessageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MessageView()
+}
